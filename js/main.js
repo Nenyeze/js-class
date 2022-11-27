b7676072d0ea1c65dd2dfc7b9f91b7a9fec1df78
Nenyeze <chinenyezefloxzy@gmail.com>
2022-11-27 05:44:42 +0100

    my-Js-class-works

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+console.log(alphabet);
+console.log(alphabet.charAt(0));
+// console.log(alphabet.charAt(alphabet.))
+console.log(alphabet.indexOf("R"))
+let name = "Nenyeze";
+let age = 12;
+let height = "12";
+let weight = "138";
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+console.log(typeof(age));
+console.log(typeof(height));
+console.log(typeof(weight));
+
+// let a = 5;
+// let b = 15;
+// let c = a+b;
+console.log(c);
+alert(c);
+alert(`the addition of ${a} and ${b} is = ${c}`)
+alert(1 + 25);
+alert(1 / 0);
+alert("B" / 2);
+
+alert(9*50);
+alert(10**10);
+alert(9.85-9);
+
+alert(9**0.5);
+ 
+
+let a = 5;
+ -(4*a*c);let b = 6;
+let c = 7;
+ alert ((-b+((b**2)-(4*a*c))**0.5)/2*a);
+ alert ((-b-((b**2)-(4*a*c))**0.5)/2*a);
+ 
+ alert((b**2)-(4*a*c));
+ let seccond = ((b**2)-(4*a*c))**0.5;
+let d=((b**2)-(4*a*c));
+console.log(d);
+console.log(d**0.5);
+alert("second"/(2*a));
+
+
+ alert(b**0.5);
+
---
+++ js_mr_Inno/master.js
@@ -0,0 +1,90 @@
+// let greeting = "Hello World";
+// console.log(greeting);
+// alert(greeting);
+// let greet = prompt("Hello world")
+
+let z;
+console.log(z);
+let name1 = "null";
+console.log(name1);
+let n = null;
+console.log(n);
+
+let w = true;
+console.log(w);
+console.log(typeof w);
+
+let v = 5.02;
+console.log(typeof v);
+
+let m="5";
+console.log(typeof m);
+// declaration of array
+let R=[]
+console.log(R);
+let R1=[5]
+console.log(R1);
+let R2=["number1",5]
+console.log(R2);
+
+let number2= `xy`;
+let R3=[number2]
+console.log(R3);
+// object declaration
+let ink={};
+console.log(typeof ink);
+let ink1={id:5};
+console.log(typeof ink1);
+console.log(ink1.id)
+let name2="nenyeze";
+let ink2={name:name2,obj:[]};
+console.log(ink2);
+
+let luv="myluv";
+let lov={myl:luv,obj:[],lev:[5]};
+console.log( lov);
+
+let ok="names";
+let ri=["nenyeze","Ada","chioma"]
+let say={nam:ok,ride:ri}
+console.log(say);
+// data type declaration
+let num1 =1;
+let num2=2;
+console.log(num1+num2);
+console.log(1+2);
+let num3=(num1+num2);
+let num4=25
+console.log(num4+num3);
+
+// let user= prompt ("please input a number");
+// casting a string-coverting a string into a number
+// let yeaa= parseInt(user);
+// let yea= (yeaa + 20);
+// console.log(yea);
+// usernumb= prompt ("");
+let a=5;
+let b="2";
+let c=2;
+console.log(a+b);
+console.log(a+c);
+console.log(typeof a+c);
+console.log((typeof a+b));
+console.log(typeof(a+c));
+console.log(typeof(a+c));
+
+// converting a number into a string
+// let giver= prompt("spell your name");
+// console.log(giver);
+// let given= parseint(giver);
+// let myname="obi";
+let g=2;
+console.log(typeof(g));
+let con= g.toString();
+console.log(con);
+console.log(typeof(con));
+
+let newuser=prompt("what is your name");
+console.log(newuser);
+let surname="Obi";
+console.log(surname,newuser);
